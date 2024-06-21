9f0ff474bab5b9540eca33b004b149e2c70f5188
petelouise <programmetress@petelouise.beauty>
2024-06-21 10:24:01 -0500

    no semis

--- main.ts
+++ main.ts
@@ -1,55 +1,55 @@
-import { Plugin } from "obsidian";
+import { Plugin } from "obsidian"
 
 export default class FocusMode extends Plugin {
-    leftSidebarWasCollapsed: boolean;
-    rightSidebarWasCollapsed: boolean;
+    leftSidebarWasCollapsed: boolean
+    rightSidebarWasCollapsed: boolean
 
     sidebarIsOpen() {
         return (
             !this.app.workspace.leftSplit.collapsed ||
             !this.app.workspace.rightSplit.collapsed
-        );
+        )
     }
 
     collapseSidebars() {
-        this.leftSidebarWasCollapsed = this.app.workspace.leftSplit.collapsed;
-        this.rightSidebarWasCollapsed = this.app.workspace.rightSplit.collapsed;
+        this.leftSidebarWasCollapsed = this.app.workspace.leftSplit.collapsed
+        this.rightSidebarWasCollapsed = this.app.workspace.rightSplit.collapsed
 
-        this.app.workspace.leftSplit.collapse();
-        this.app.workspace.rightSplit.collapse();
+        this.app.workspace.leftSplit.collapse()
+        this.app.workspace.rightSplit.collapse()
     }
 
     restoreSidebars() {
         if (!this.leftSidebarWasCollapsed) {
-            this.app.workspace.leftSplit.expand();
+            this.app.workspace.leftSplit.expand()
         }
 
         if (!this.rightSidebarWasCollapsed) {
-            this.app.workspace.rightSplit.expand();
+            this.app.workspace.rightSplit.expand()
         }
     }
 
     toggleSidebars() {
         if (this.sidebarIsOpen()) {
-            this.collapseSidebars();
+            this.collapseSidebars()
         } else {
-            this.restoreSidebars();
+            this.restoreSidebars()
         }
     }
 
     async onload() {
-        console.log("loading sidebar toggle plugin ...");
+        console.log("loading sidebar toggle plugin ...")
 
         this.addCommand({
             id: "toggle-sidebars",
             name: "Toggle Sidebars",
             callback: () => {
-                this.toggleSidebars();
+                this.toggleSidebars()
             },
-        });
+        })
     }
 
     onunload() {
-        console.log("unloading sidebar toggle plugin ...");
+        console.log("unloading sidebar toggle plugin ...")
     }
 }
